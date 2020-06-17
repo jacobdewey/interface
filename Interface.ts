b8b92cf44e78cb68ef2b3b592728e26ce0f66fb6
jacobdewey <66138103+jacobdewey@users.noreply.github.com>
2020-06-17 10:26:36 -0400

    Update pxt.json, main.blocks, main.ts, Interface.ts

---
+++ Interface.ts
@@ -0,0 +1,73 @@
+// Add your code here
+namespace interfaces{
+export declare interface cars{
+"yourInvenory":{
+"yourInvenoryPassword": 12345,
+"yourCars":{
+"car_one":{
+"Make": "Tesla",
+"YourTesla":{
+"Model": "X",
+}
+}
+"car_two":{
+"Make": "Tesla",
+"YourTesla":{
+"Model": "S"
+}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
